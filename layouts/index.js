26a05ba0e132dca6946c0a247006f76c237bbfee
Derek <xuderekq@hotmail.com>
2017-07-21 10:24:51 +0800

    Responsive design
    Added a sidebar for mobile users

--- layouts/index.js
+++ layouts/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 import PropTypes from "prop-types"
 import Link from "gatsby-link"
 import Helmet from "react-helmet"
-import { Menu, Container, Icon } from 'semantic-ui-react'
+import { Menu, Container, Icon, Grid, Button, Segment, Header, Sidebar } from 'semantic-ui-react'
 
 //import '../semantic/dist/semantic.min.css';
 
@@ -12,12 +12,19 @@ export default class Template extends React.Component {
   }
   render() {
     return (
-      <div>
-        <Navbar />
-        <Container>
-          {this.props.children()}
-        </Container>
-      </div>
+        <Grid>
+          <Grid.Row only='mobile' stretched>
+            <Grid.Column>
+              <SidebarMobile pages={this.props.children}/>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row only='computer tablet'>
+            <Navbar />
+            <Container style={{paddingTop: 60}}>
+              {this.props.children()}
+            </Container>
+          </Grid.Row>
+        </Grid>
     )
   }
 }
@@ -30,7 +37,6 @@ class Navbar extends Component {
   render() {
     const { activeItem } = this.state
     return (
-      <div style={{marginBottom: 60}}>
       <Menu color='blue' fixed='top' inverted>
         <Menu.Item header as={Link} to="/" onClick={this.handleItemClick}>Derek Xu</Menu.Item>
         <Menu.Item as={Link} to="/resume" name='resume' active={activeItem === 'resume'} onClick={this.handleItemClick}>
@@ -50,6 +56,65 @@ class Navbar extends Component {
           About
         </Menu.Item>
       </Menu>
+    )
+  }
+}
+
+class SidebarMobile extends Component {
+  state = { visible: false }
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name })
+    this.toggleVisibility()
+  } 
+
+  render() {
+    const { visible, activeItem } = this.state
+    return (
+      <div>
+        <Grid.Row>
+        <Menu fixed='top'>
+          <Menu.Item onClick={this.toggleVisibility}>
+            <Icon name='sidebar' />
+          </Menu.Item>
+          <Menu.Item header>
+            Derek Xu
+          </Menu.Item>
+        </Menu>
+        </Grid.Row>
+        <Grid.Column stretched>
+        <Sidebar.Pushable style={{marginTop:40}}>
+          <Sidebar as={Menu} animation='push' width='thin' visible={visible} icon='labeled' vertical inverted>
+            <Menu.Item header as={Link} to="/" onClick={this.handleItemClick}>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item as={Link} to="/resume" name='resume' active={activeItem === 'resume'} onClick={this.handleItemClick}>
+              <Icon name='file text' />
+              Resume
+            </Menu.Item>
+            <Menu.Item as={Link} to="/projects" name='projects' active={activeItem === 'projects'} onClick={this.handleItemClick}>
+              <Icon name='wrench' />
+              Projects
+            </Menu.Item>
+            <Menu.Item as={Link} to="/music" name='music' active={activeItem === 'music'} onClick={this.handleItemClick}>
+              <Icon name='music' />
+              Music
+            </Menu.Item>
+            <Menu.Item as={Link} to="/about" name='about' active={activeItem === 'about'} onClick={this.handleItemClick}>
+              <Icon name='user circle outline' />
+              About
+            </Menu.Item>
+          </Sidebar>
+            <Sidebar.Pusher style={{minHeight:'100vh'}}>
+              <Container style={{paddingTop:20}}>
+                {this.props.pages()}
+              </Container>
+            </Sidebar.Pusher>
+        </Sidebar.Pushable>
+        </Grid.Column>
       </div>
     )
   }
