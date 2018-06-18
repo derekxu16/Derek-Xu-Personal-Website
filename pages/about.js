235ebc3610278a17c71085d5f1eea740a3a61dde
Derek <xuderekq@hotmail.com>
2018-06-17 21:29:46 -0400

    Redesigned about page

--- pages/about.js
+++ pages/about.js
@@ -1,23 +1,53 @@
-import React from "react"
+import React, { Component } from "react"
 import Link from "gatsby-link"
+import DimTile from '../components/DimTile'
+import Tile from '../components/Tile'
+import TileContainer from '../components/TileContainer'
+import { withPrefix } from 'gatsby-link'
 import Helmet from "react-helmet"
-import { Container, Header, Divider, List } from 'semantic-ui-react'
-import DimButton from '../components/DimTile'
 
-export default class About extends React.Component {
+export default class Resume extends React.Component {
+  constructor() {
+    super();
+    this.state = { headerPadding: 0, header: '' };
+  }
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener("resize", this.updateDimensions);
+  }
+
+  updateDimensions = () => {
+    const mobile = window.innerWidth < 720;
+    let state = {style: {background:'linear-gradient(to right, #4267b2, #92a2dd)', fontSize: 22, height:'100%', width:'100%'}}
+    if (mobile) {
+      state.style['justifyContent'] = 'flex-start';
+      this.setState(Object.assign(state,{
+        headerPadding: '52px 20px 0px 20px',
+        header: 'Tap on any of the tiles to learn more about my interests and lifestyle!',
+      }));
+    } else {
+      this.setState(Object.assign(state,{
+        headerPadding: '0px 20px 0px 20px',
+        header: 'Hover over any of the tiles to learn more about my interests and lifestyle!',
+      }));
+    }
+  }
   render() {
     return (
-      <div style={{display:'flex', flexDirection:'column', justifyContent:'center', fontSize:'calc(16px + 1vw)', background:'linear-gradient(to bottom right, #4267b2, #92a2dd)', width:'100%', minHeight:'100%', paddingTop:40}}>
-        <div className='textbox' style={{color:'#ededed', flex:'auto', maxWidth:700}}>
-          <p> I'm an 18 year old currently studying Software Engineering at the University of Waterloo. I grew up in Ottawa, Ontario and completed the IB Diploma Programme at Colonel By SS. </p>
-          <p> I love programming! I currently like working with React and C++ the most. I also love doing competitve programming contests. I am ranked in the top 20% of users on CSAcademy and in the top 100 users on the Don Mills Online Judge. </p>
-          <p> Outside of technology, I've got two other major hobbies. I have always loved music! I can play several instruments but I sing and play guitar most frequently. I also love to yoyo. It may sound outdated but yoyoing has changed a lot since the '90s! Yoyoing requires technical skill, creativity and determination, it's so much fun! </p>
-        </div>
-        <div className='stackable' style={{flex:'auto', width:'100%'}}>
-          <DimButton name='Music' icon='youtube' color='#cc181e' message='Click here to visit my music channel' to="https://www.youtube.com/derekxumusic"/>
-          <DimButton name='Yoyoing' icon='instagram' color='#8a3ab9' message='Click here to see my yoyo videos on Instagram' to='https://www.instagram.com/yoderekyo/'/>
-          <DimButton name='Competitive Programming' icon='winner' color='#4875B4' message='Click here to view my DMOJ profile' to='https://dmoj.ca/user/plumsauc3/'/>
-        </div>
+      <div className='vertical-center-container' style={this.state.style}>
+        <p className="tile-container-header" style={{ padding: this.state.headerPadding }}>
+          {this.state.header}
+        </p>
+        <TileContainer>
+          <DimTile name='Education' icon='graduation' color='#e4b429' message="I'm about to begin my 2A term of SE at Waterloo. Prior to this, I completed the IB program at Colonel By SS."/>
+          <DimTile name='Blog' icon='medium' color='#03a87c' message="I write about anything that I personally believe to be worth sharing on my Medium blog. Click here to check it out, hopefully you'll find an article that you enjoy!" to="https://medium.com/@derekxu"/>
+          <DimTile name='Music Covers' icon='youtube' color='#cc181e' message='I love to sing and play guitar. Whenever I find the time, I like to record covers of my favourite songs on my Youtube channel!' to="https://www.youtube.com/derekxumusic"/>
+          <DimTile name='Yoyoing' icon='instagram' color='#8a3ab9' message="You've most likely never seen modern yoyo play before. It's extremely fun, challenging and allows me to express my creativity. Click here to see my yoyo videos on Instagram." to='https://www.instagram.com/yoderekyo/'/>
+          <DimTile name='Music Reviews' icon='music' color='#aac' message='Not only do I love performing, I also love to listen to music and analyze it. You can get a good idea of what my music taste is like by checking out my RateYourMusic profile.' to='https://rateyourmusic.com/~derekxu'/>
+          <DimTile name='Competitive Programming' icon='winner' color='#5575CC' message='I enjoy solving algorithmic problems and like to compete in contests. You can view some of the problems that I have solved on my DMOJ profile.' to='https://dmoj.ca/user/plumsauc3'/>
+          {/* <DimTile name='SQL' icon='database' color='#D14F52' message='I am fluent in SQL and can confidently manage relational databases.' />
+          <DimTile name='Python' icon='python' color='#ffd343' message="I have experience building backends with Python using Flask and Django. I am also comfortable doing data analysis and manipulation with NumPy, SciPy and Pandas." /> */}
+        </TileContainer>
       </div>
     )
   }
